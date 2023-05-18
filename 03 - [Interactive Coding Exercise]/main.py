354af394086a35be1c1b5c71add2ca4881aea046
Dexxter <99261879+DexxterGWM@users.noreply.github.com>
2023-05-18 11:03:25 -0300

    main.py File

---
+++ 03 - [Interactive Coding Exercise]/main.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇🏻
+year = int(input(' Which year do you want to check? '))
+# 🚨 Don't change the code above 👆🏻
+
+# Write your code below this line 👇🏻
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(' Leap year.')
+        else:
+            print(' Not leap year.')
+    else:
+        print(' Leap year.')
+else:
+    print(' Not leap year.')
+# Write your code above this line 👆🏻
