7ee7535212fe8b9960b348e21cdaff26f98fa7f2
Dexxter <99261879+DexxterGWM@users.noreply.github.com>
2023-05-18 11:02:28 -0300

    main.py File

---
+++ 01 - [Interactive Coding Exercise]/main.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇🏻
+number = int(input(' Which number do you want to check? '))
+# 🚨 Don't change the code above 👆🏻
+
+# Write your code below this line 👇🏻
+if number % 2 == 0:
+    print(' This is a even number.')
+else:
+    print(' This is an odd number.')
+# Write your code above this line 👆🏻
